c9d3bf3904d6ab4e96651c20da4647a048c8a2ca
Craig <craigmiller160@gmail.com>
2021-12-29 18:34:00 -0500

    MOre work on lib

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,19 @@
+const { createTransformer } = require('ts-jest');
+
 module.exports = {
-    collectCoverageFrom: [
-        '<rootDir>/src/**/*.{ts,tsx}',
-        '<rootDir>/src/**/*.d.ts'
-    ],
-    testMatch: [
-        '<rootDir>/test/**/*.{test,Spec}.{ts,tsx}'
-    ],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest'
-    },
-    globals: {
-        'ts-jest': {
-            tsconfig: '<rootDir>/tsconfig.json'
-        }
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '<rootDir>/src/**/*.d.ts'
+  ],
+  testMatch: [
+    '<rootDir>/test/**/*.{test,Spec}.{ts,tsx}'
+  ],
+  transform: {
+    '^.+\\.tsx?$': createTransformer()
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.json'
     }
+  }
 };
