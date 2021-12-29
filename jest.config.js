e8d6d2907b1b95ca74f3d679d58ca28fccc20bd4
Craig <craigmiller160@gmail.com>
2021-12-29 18:13:43 -0500

    Setup jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,17 @@
-
 module.exports = {
-
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.{ts,tsx}',
+        '<rootDir>/src/**/*.d.ts'
+    ],
+    testMatch: [
+        '<rootDir>/test/**/*.{test,Spec}.{ts,tsx}'
+    ],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    },
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.json'
+        }
+    }
 };
